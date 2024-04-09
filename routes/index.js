c7e7e564ddca15000ba37e9d893d10c091d0b580
Peng Yu <yp1990@gmail.com>
2024-04-09 12:41:22 +0100

    add apis

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
+var axios = require('axios');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Home | EtherAid' });
+});
+
+/* GET about page. */
+router.get('/news', async function (req, res, next) {
+
+
+  const options = {
+    method: 'GET',
+    url: 'https://google-news13.p.rapidapi.com/latest',
+    params: { lr: 'en-US' },
+    headers: {
+      'X-RapidAPI-Key': '108f64079bmshe074fe22625578bp1bd533jsn6eb95e1e42ac',
+      'X-RapidAPI-Host': 'google-news13.p.rapidapi.com'
+    }
+  };
+
+  let news;
+
+  try {
+    response = await axios.request(options);
+    news = response.data.items;
+    console.log(news);
+  } catch (error) {
+    console.error(error);
+  }
+  res.render('news', { title: 'News | EtherAid', "news": news });
 });
 
 /* GET address page. */
